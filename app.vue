06116308f87efa073b209d93d9e4d61439402d83
Artem <Artteem8@gmail.com>
2024-05-21 13:32:19 +0200

    Added a navbar

--- app.vue
+++ app.vue
@@ -1,6 +1,23 @@
 <template>
-  <NuxtLoadingIndicator/>
   <div>
-    <NuxtPage />
+    <NuxtLoadingIndicator/>
+    <!-- navBar -->
+    <nav class="bg-blue-500 p-4">
+      <ul class="flex">
+        <li class="mr-6">
+          <router-link class="text-white text-lg hover:text-gray-300" to="/">Home</router-link>
+        </li>
+        <li class="mr-6">
+          <router-link class="text-white text-lg hover:text-gray-300" to="/about">About</router-link>
+        </li>
+        <li class="mr-6">
+          <router-link class="text-white text-lg hover:text-gray-300" to="/contact">Contact</router-link>
+        </li>
+      </ul>
+    </nav>
+    <!-- Content -->
+    <div>
+      <NuxtPage />
+    </div>
   </div>
 </template>
